730577951373dcdfb003fa046aa04bdc66059202
Francois <contact.fpweb@gmail.com>
2022-08-01 19:25:55 +0200

    fix form

--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,18 @@
   <main>
     <form name="form-name" value="contact" method="POST" data-netlify="true" action="/success" netlify-honeypot="bot-field">
       <p class="hidden">
-        <label>
-          Don’t fill this out if you’re human: <input name="bot-field" />
-        </label>
+       <input type="hidden" name="bot-field" />
       </p>
       <input type="hidden" name="form-name" value="contact">
       <p>
-        <label>Your Name: <input type="text" name="name" /></label>
+        <label>Your Name: <input type="text" name="name" required/></label>
       </p>
       <p>
-        <label>Your Email: <input type="email" name="email" /></label>
+        <label>Your Email: <input type="email" name="email" required/></label>
       </p>
       <p>
-        <label>Message: <textarea name="message"></textarea></label>
+        <label>Message: <textarea name="message" required></textarea></label>
       </p>
-      <div data-netlify-recaptcha="true"></div>
       <p>
         <button type="submit">Send</button>
       </p>
