95acc60d9eef142e60ce15f638458a1e7958cb9e
Francois <contact.fpweb@gmail.com>
2022-08-01 19:45:22 +0200

    retry subject

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <form name="contact" method="POST" data-netlify="true" action="/success" netlify-honeypot="bot-field">
       <input type="hidden" name="bot-field" />
       <input type="hidden" name="form-name" value="contact">
-      <input type="hidden" name="subject" value="Vous avez reçu un nouveau messag" />
+      <input type="hidden" name="subject" value="Vous avez reçu un nouveau message" />
       <p>
         <label>Your Name: <input type="text" name="name" required/></label>
       </p>
